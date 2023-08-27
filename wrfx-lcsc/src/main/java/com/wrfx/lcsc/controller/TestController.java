62d7d218c19816a6da789509a76a43208e388bb2
WenRenFengXuan <xiaobang0306@163.com>
2023-08-27 19:02:31 +0800

    1.新增jasypt加密解密测试 2.新增eureka注册中心配置

--- wrfx-lcsc/src/main/java/com/wrfx/lcsc/controller/TestController.java
+++ wrfx-lcsc/src/main/java/com/wrfx/lcsc/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.wrfx.lcsc.controller;
 
 import com.wrfx.chk.pojo.response.CommonResponse;
+import com.wrfx.chk.utils.JasyptUtils;
 import com.wrfx.lcsc.bean.vo.BaseProductVo;
 import com.wrfx.lcsc.service.TestService;
 import io.swagger.annotations.Api;
@@ -23,6 +24,29 @@ public class TestController {
     @ApiOperation(value = "删除人脸",notes = "根据id进行删除")
     public CommonResponse<String> addTest(@RequestBody BaseProductVo baseProductVo){
         testServiceImpl.addTest(baseProductVo);
+        return new CommonResponse<>("success");
+    }
+
+
+    @GetMapping("/encryptWithMD5")
+    @ApiOperation(value = "解密测试",notes = "解密测试")
+    public CommonResponse<String> encryptWithMD5(){
+        String passwork = "wrfx_chk_passwork";
+        System.out.println(JasyptUtils.encryptWithMD5("jdbc:mysql://localhost:3306/myTest?serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=utf-8", passwork));
+        System.out.println(JasyptUtils.encryptWithMD5("root", passwork));
+        System.out.println(JasyptUtils.encryptWithMD5("chk@1234", passwork));
+        System.out.println(JasyptUtils.encryptWithMD5("com.mysql.cj.jdbc.Driver", passwork));
+        return new CommonResponse<>("success");
+    }
+
+    @GetMapping("/decryptWithMD5")
+    @ApiOperation(value = "解密测试",notes = "解密测试")
+    public CommonResponse<String> decryptWithMD5(){
+        String passwork = "wrfx_chk_passwork";
+        System.out.println(JasyptUtils.decryptWithMD5("uxh7eZpjSnp9tA6RefuyLKU9jzJ3AJDGoMY+9gX8lbfub+AA9LV1tdNwkQB4lFzj4sLmpvnIHu0u6VekcrCX2jaUCX9ERZW9NKs6SerWpXyDNxpX3KNHhrE/jv3XBsEhIMFzp5vwXxtPYQhM9Jv/SQ==", passwork));
+        System.out.println(JasyptUtils.decryptWithMD5("SHrZQhaW04r+lQpLfE7tRg==", passwork));
+        System.out.println(JasyptUtils.decryptWithMD5("sYVDJXgiTFHMnSdiXHi9LwghWJR0HlVQ", passwork));
+        System.out.println(JasyptUtils.decryptWithMD5("q4gMZxhWLcT717zbCOpkKNYRFEt/PsheDlk3eTyoMubHRvuGrbTh9w==", passwork));
         return new CommonResponse<>("success");
     }
 }
